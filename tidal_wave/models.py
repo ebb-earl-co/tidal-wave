4e7602d0204c8d1ab73345c00256895a733c2dc8
Colin Coe <ebb-earl-co@pm.me>
2024-03-15 21:36:33 -0600

    Fix typo in models.py

--- tidal_wave/models.py
+++ tidal_wave/models.py
@@ -1,7 +1,7 @@
 import base64
 from dataclasses import dataclass, field
 from datetime import date, datetime
-from io import BytessIO
+from io import BytesIO
 import logging
 from pathlib import Path
 import re
