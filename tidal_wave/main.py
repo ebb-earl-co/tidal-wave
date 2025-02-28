7728f2da3765776d41f7fd53fade62cd1cd14927
Colin Coe <ebb-earl-co@pm.me>
2025-02-28 09:41:32 -0700

    Bump version of 'tidal-wave' to 2025.2.1.

--- tidal_wave/main.py
+++ tidal_wave/main.py
@@ -29,7 +29,7 @@ from .track import Track
 from .utils import is_tidal_api_reachable
 from .video import Video
 
-__version__ = "2024.11.1"
+__version__ = "2025.2.1"
 
 
 # https://typer.tiangolo.com/tutorial/options/version/#fix-with-is_eager
