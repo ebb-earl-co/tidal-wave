6a97e19cc71dba8f6cbf7d98724723287c15b1b2
Colin Coe <ebb-earl-co@pm.me>
2026-02-26 14:05:33 -0700

    Hotfix for dependabot failures (#304)
    * Modify YAML files, updating versions and using 'uv'.
    
    * Gate publishing when not 'workflow_dispatch'.
    
    * Use actions setup-python for system Python 3.14.0.
    
    * Bump minimum required Python to 3.10 for platformdirs.
    
    * Update uv.lock after bumping required-python to 3.10+.
    
    * Use 'uv' in Ubuntu 22.0 PyInstaller Action.
    
    * Update PyInstaller command in Ubuntu Actions.
    
    * Bump to Python 3.14.3 and use 'uv' in all Actions.
    
    * Fix path for 'ruff' to format in python-lint.yml.
    
    * Pin setup-uv action to v7.3.0 in all Action YAMLs.
    
    * Remove redundant login.AudioFormat.
    
    * Fix urllib3 typo in pyproject.toml.
    
    * Bump versions in pre-commit config.

--- hooks/version-match.py
+++ hooks/version-match.py
@@ -3,21 +3,21 @@
 from pathlib import Path
 from re import Match, search
 from sys import exit, stderr, version_info
-from typing import Any, Dict, Optional, Tuple
+from typing import Any
 
 if __name__ == "__main__":
-    vi: Tuple[int, int] = version_info[:2]
+    vi: tuple[int, int] = version_info[:2]
     if vi < (3, 11):
         from pip._vendor import tomli as tomllib
     else:
         import tomllib
 
-    pyproject_toml: Dict[str, Any] = tomllib.loads(Path("pyproject.toml").read_text())
-    pyproject_toml_version: Optional[str] = pyproject_toml.get(
+    pyproject_toml: dict[str, Any] = tomllib.loads(Path("pyproject.toml").read_text())
+    pyproject_toml_version: str | None = pyproject_toml.get(
         "project", {"version": None}
     ).get("version")
 
-    pyinstaller_version_match: Optional[Match] = search(
+    pyinstaller_version_match: Match | None = search(
         pyproject_toml_version, Path("pyinstaller.py").read_text()
     )
     if pyinstaller_version_match is None:
